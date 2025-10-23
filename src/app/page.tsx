6efb4ece38c199939a502a2418a03dc717df6c7c
utsavg05 <utsav.gp1204@gmail.com>
2025-10-23 10:24:26 +0530

    Implement Task Manager component and Auth component; refactor Home page to use TaskManager

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,85 +1,10 @@
-"use client"
-import Image from "next/image";
-import { useState } from "react";
+import TaskManager from '@/components/task-manager'
+import React from 'react'
 
-export default function Home() {
-  const[newTask, setNewTask] = useState({title: "", description: ""})
-
-  const handleSubmit = async () => {
-
-  }
-
-  const handleFileChange = () => {
-
-  }
-
+const Home = () => {
   return (
-    <div style={{ maxWidth: "600px", margin: "0 auto", padding: "1rem" }}>
-      <h2>Task Manager CRUD</h2>
-
-      {/* Form to add a new task */}
-      <form onSubmit={handleSubmit} style={{ marginBottom: "1rem" }}>
-        <input
-          type="text"
-          placeholder="Task Title"
-          onChange={(e) =>
-            setNewTask((prev) => ({ ...prev, title: e.target.value }))
-          }
-          style={{ width: "100%", marginBottom: "0.5rem", padding: "0.5rem" }}
-        />
-        <textarea
-          placeholder="Task Description"
-          onChange={(e) =>
-            setNewTask((prev) => ({ ...prev, description: e.target.value }))
-          }
-          style={{ width: "100%", marginBottom: "0.5rem", padding: "0.5rem" }}
-        />
-
-        <input type="file" accept="image/*"  />
-
-        <button type="submit" style={{ padding: "0.5rem 1rem" }}>
-          Add Task
-        </button>
-      </form>
-
-      {/* List of Tasks */}
-      {/* <ul style={{ listStyle: "none", padding: 0 }}>
-        {tasks.map((task, key) => (
-          <li
-            key={key}
-            style={{
-              border: "1px solid #ccc",
-              borderRadius: "4px",
-              padding: "1rem",
-              marginBottom: "0.5rem",
-            }}
-          >
-            <div>
-              <h3>{task.title}</h3>
-              <p>{task.description}</p>
-              <img src={task.image_url} style={{ height: 70 }} />
-              <div>
-                <textarea
-                  placeholder="Updated description..."
-                  onChange={(e) => setNewDescription(e.target.value)}
-                />
-                <button
-                  style={{ padding: "0.5rem 1rem", marginRight: "0.5rem" }}
-                  onClick={() => updateTask(task.id)}
-                >
-                  Edit
-                </button>
-                <button
-                  style={{ padding: "0.5rem 1rem" }}
-                  onClick={() => deleteTask(task.id)}
-                >
-                  Delete
-                </button>
-              </div>
-            </div>
-          </li>
-        ))}
-      </ul> */}
-    </div>
-  );
+    <TaskManager />
+  )
 }
+
+export default Home
